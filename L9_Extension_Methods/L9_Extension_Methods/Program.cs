c8999271aa29717ea40d88e5bbe1adf79c79e1d4
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-04 17:07:57 -0500

    lesson 9 - extension methods

---
+++ L9_Extension_Methods/L9_Extension_Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9_Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long post blah blah blah";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+        }
+    }
+}
