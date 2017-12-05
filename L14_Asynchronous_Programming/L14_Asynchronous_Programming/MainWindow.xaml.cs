96e8997834b2b8616e83d431556a17a9ffb11115
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-05 15:35:52 -0500

    lesson 14 - asynchronous programming

---
+++ L14_Asynchronous_Programming/L14_Asynchronous_Programming/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L14_Asynchronous_Programming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var html = await GetHtmlAsync("http://msdn.microsoft.com");
+            MessageBox.Show(html.Substring(0, 20));
+        }
+
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+    }
+}
