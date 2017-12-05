b4e830d467795348566d48d98246541eb745827b
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-12-04 20:21:12 -0500

    lesson 11 - nullable types

---
+++ L11_Nullable_Types/L11_Nullable_Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L11_Nullable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            DateTime date2 = date ?? DateTime.Today;
+
+            DateTime date3 = (date != null) ? date.GetValueOrDefault() : DateTime.Today;
+
+            Console.WriteLine(date2);
+        }
+    }
+}
